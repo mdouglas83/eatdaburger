e1dfef6d2588617ee3cacc119bbc08a58b2d626a
MDOUGLAS83 <mrdouglas83@gmail.com>
2018-03-21 15:56:29 -0500

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -12,17 +12,49 @@ const cnxn = [
   }, 
   {
     port: 3306,
-    host: "us-cdbr-iron-east-05.cleardb.net",
-    user: "b54acb56edc97a",
-    password: "f625417e",
-    database: "heroku_0763d0b06d53eb6"
+    host: "ixqxr3ajmyapuwmi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "s0sv82h9wmevzdzn",
+    password: "uzks6dnbpib7iq4k",
+    database: "burger_db"
   }
 ];
 
 var connection = mysql.createConnection(cnxn[Z]);
 connection.connect(function(err) {
   if (err) return console.error("error connecting: " + err.stack);
-  console.log("connected to mysql://" + cnxn[Z].host + ":" + cnxn[Z].port + " (thread " + connection.threadId + ")");
+  connection.query("CREATE DATABASE IF NOT EXISTS burger_db;", function(err) {
+    if (err) return console.error("error: could not create burger_db");
+    connection.query("USE burger_db;", function(err) {
+      if (err) return console.error("error: could not switch to burger_db");
+      console.log("connected to mysql://" + cnxn[Z].host + ":" + cnxn[Z].port + " (thread " + connection.threadId + ")");
+      connection.query(
+        "CREATE TABLE IF NOT EXISTS burgers (" +
+        "id int NOT NULL AUTO_INCREMENT, " +
+        "burger varchar(255) NOT NULL, " +
+        "eaten BOOLEAN DEFAULT false, " +
+        "PRIMARY KEY (id));", function(err) {
+        if (err) return console.error("error: could not create table burgers");
+        connection.query("SELECT COUNT(*) FROM burgers;", function(err, result) {
+          if (err) return console.error("error: could not retrive record count from burgers");
+          // TEMP TEMP TEMP
+          console.log(result);
+          if (result === 0) {
+            var sql = "INSERT INTO burgers (burger, eaten) VALUES ?;";
+            var values = [
+              ['Big Mac', false],
+              ['Quarter Pounter', false],
+              ['Cajun Chicken Sandwich', false],
+              ['Belly Burger', true]
+            ];
+            connection.query(sql, values, function(err, result) {
+              if (err) return console.error("error: could not insert records into burgers");
+              console.log("inserted " + result.affectedRows + " into burgers");
+            });
+          }
+        });
+      });
+    });
+  });
 });
 
 module.exports = connection;
